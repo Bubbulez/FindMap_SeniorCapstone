03db6345c543c7ee77cf4b01aaa0cd1004c32526
Nahla Wright <88988827+NahlaWright@users.noreply.github.com>
2026-03-04 19:58:29 +0000

    comp

---
+++ findmap/pages/locations.js
@@ -0,0 +1,26 @@
+export const locations = [
+  {
+    title: "Downs Hall",
+    address: "Kean University, 1000 Morris Ave, Union, NJ 07083",
+    embedUrl: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.598049991676!2d-74.23883032393073!3d40.68282457139806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5ca13190f5%3A0x1a26549323e715a7!2sDowns%20Hall!5e0!3m2!1sen!2sus!4v1772483334792!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+  }, 
+  {
+    title: "North Avenue Academic Building [NAAB]",
+    address: "New Jersey Center for Science, Technology & Mathematics, 1000 Morris Ave, Union, NJ 07083",
+    embedUrl: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.8861157786637!2d-74.23112702393108!3d40.676478171399076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5ca13190f5%3A0xd36b39cdf932d2f1!2sNorth%20Avenue%20Academic%20Building%20%5BNAAB%5D!5e0!3m2!1sen!2sus!4v1772483371762!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+  },
+  {
+    title: "Green Lane Academic Building [GLAB]",
+    address: "1040 Morris Ave, Union, NJ 07083",
+    embedUrl: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.598049991676!2d-74.23883032393074!3d40.68282457139806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad435b4c0dfd%3A0x95ccbf1c2ffea977!2sGreen%20Lane%20Academic%20Building%20%5BGLAB%5D!5e0!3m2!1sen!2sus!4v1772483388425!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+  },
+  { 
+    title: "Miron Student Center",
+    address: "Kean University, 1000 Morris Avenue Kean University, Miron Center, 1000 Morris Ave, Union, NJ 07083",
+    embedUrl: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.826931311723!2d-74.23692725762217!3d40.677782135800825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5bf222f1e1%3A0x1ecd60c11b43f464!2sMiron%20Student%20Center!5e0!3m2!1sen!2sus!4v1772652498649!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+  },
+  {
+    title: "Nancy Thompson Library",
+    address: ""
+  },
+];
