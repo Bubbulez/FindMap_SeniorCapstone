a90cfb884b6141011b7c8b1bacc2744c20fba9a5
sydhan47-beep <emmanuelscamba47@gmail.com>
2026-03-25 19:26:48 +0000

    Made Bulding page

---
+++ findmap/app/buildings/page.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    <div>
+      <header>
+        <h1>Buildings</h1>
+      </header>
+
+      <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+      </nav>
+
+      <main>
+        <p>Welcome to my Buildings</p>
+      </main>
+
+      <footer>
+        <p>© 2026</p>
+      </footer>
+    </div>
+  );
+}
