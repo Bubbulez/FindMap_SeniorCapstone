32b17c5043278e474d5b0add4067960812318381
Deli <A.Delali.Doe@gmail.com>
2026-03-30 21:10:41 +0000

    F

--- findmap/page.tsx
+++ findmap/page.tsx
@@ -1,61 +1,23 @@
-<div style={{ display: "flex", gap: "10px", marginTop: "20px" }}>
-  <Link
-    href="/events"
-    style={{
-      padding: "10px 20px",
-      borderRadius: "20px",
-      backgroundColor: "#4a8df6",
-      color: "white",
-      textDecoration: "none",
-      fontWeight: "bold",
-      display: "inline-block"
-    }}
-  >
-    Events
-  </Link>
+import Link from "next/link";
 
-  <Link
-    href="/buildings"
-    style={{
-      padding: "10px 20px",
-      borderRadius: "20px",
-      backgroundColor: "white",
-      color: "black",
-      textDecoration: "none",
-      fontWeight: "bold",
-      display: "inline-block"
-    }}
-  >
-    Buildings
-  </Link>
+export default function Home() {
+  return (
+    <div style={{ display: "flex", gap: "10px", marginTop: "20px" }}>
+      <Link href="/events" style={{ padding: "10px 20px", borderRadius: "20px", backgroundColor: "#4a8df6", color: "white", textDecoration: "none", fontWeight: "bold", display: "inline-block" }}>
+        Events
+      </Link>
 
-  <Link
-    href="/dining"
-    style={{
-      padding: "10px 20px",
-      borderRadius: "20px",
-      backgroundColor: "white",
-      color: "black",
-      textDecoration: "none",
-      fontWeight: "bold",
-      display: "inline-block"
-    }}
-  >
-    Dining
-  </Link>
+      <Link href="/buildings" style={{ padding: "10px 20px", borderRadius: "20px", backgroundColor: "white", color: "black", textDecoration: "none", fontWeight: "bold", display: "inline-block" }}>
+        Buildings
+      </Link>
 
-  <Link
-    href="/clubs"
-    style={{
-      padding: "10px 20px",
-      borderRadius: "20px",
-      backgroundColor: "white",
-      color: "black",
-      textDecoration: "none",
-      fontWeight: "bold",
-      display: "inline-block"
-    }}
-  >
-    Clubs
-  </Link>
-</div>
+      <Link href="/dining" style={{ padding: "10px 20px", borderRadius: "20px", backgroundColor: "white", color: "black", textDecoration: "none", fontWeight: "bold", display: "inline-block" }}>
+        Dining
+      </Link>
+
+      <Link href="/clubs" style={{ padding: "10px 20px", borderRadius: "20px", backgroundColor: "white", color: "black", textDecoration: "none", fontWeight: "bold", display: "inline-block" }}>
+        Clubs
+      </Link>
+    </div>
+  );
+}
