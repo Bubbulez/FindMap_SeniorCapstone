cd97a4fa86fbf1dcae1cc43adbcc4b96bbc66fdf
Nahla Wright <88988827+NahlaWright@users.noreply.github.com>
2026-03-30 19:00:27 +0000

    added clickable locations list and fixed iframe strings

--- findmap/pages/locations.js
+++ findmap/pages/locations.js
@@ -2,12 +2,12 @@ export const locations = [
   {
     title: "Downs Hall",
     address: "Kean University, 1000 Morris Ave, Union, NJ 07083",
-    embedUrl: <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.598049991676!2d-74.23883032393073!3d40.68282457139806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5ca13190f5%3A0x1a26549323e715a7!2sDowns%20Hall!5e0!3m2!1sen!2sus!4v1772483334792!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    embedUrl: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.598049991676!2d-74.23883032393073!3d40.68282457139806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5ca13190f5%3A0x1a26549323e715a7!2sDowns%20Hall!5e0!3m2!1sen!2sus!4v1772483334792!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
   }, 
   {
     title: "North Avenue Academic Building [NAAB]",
     address: "New Jersey Center for Science, Technology & Mathematics, 1000 Morris Ave, Union, NJ 07083",
-    embedUrl: <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.8861157786637!2d-74.23112702393108!3d40.676478171399076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5ca13190f5%3A0xd36b39cdf932d2f1!2sNorth%20Avenue%20Academic%20Building%20%5BNAAB%5D!5e0!3m2!1sen!2sus!4v1772483371762!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    embedUrl: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.8861157786637!2d-74.23112702393108!3d40.676478171399076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5ca13190f5%3A0xd36b39cdf932d2f1!2sNorth%20Avenue%20Academic%20Building%20%5BNAAB%5D!5e0!3m2!1sen!2sus!4v1772483371762!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
   },
   {
     title: "Green Lane Academic Building [GLAB]",
@@ -22,36 +22,36 @@ export const locations = [
   {
     title: "Nancy Thompson Library",
     address: "1000 Morris Ave, Union, NJ 07083",
-    embedUrl: <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6051.631655184196!2d-74.23595892393101!3d40.67802677139868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5ca13190f5%3A0x74b57d6eb308f6c3!2sNancy%20Thompson%20Library!5e0!3m2!1sen!2sus!4v1772654487608!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    embedUrl: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6051.631655184196!2d-74.23595892393101!3d40.67802677139868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5ca13190f5%3A0x74b57d6eb308f6c3!2sNancy%20Thompson%20Library!5e0!3m2!1sen!2sus!4v1772654487608!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
   },
   {
     title: "Center for Academic Success [CAS]",
     address:"Kean University, Center for Academic Success, 1000 Morris Ave, Union, NJ 07083",
-    embedUrl: <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6051.581436593685!2d-74.23681722393101!3d40.67857997139864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5ca13190f5%3A0xd8ccec80658d4f7f!2sCenter%20for%20Academic%20Success%20%5BCAS%5D!5e0!3m2!1sen!2sus!4v1773685994760!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    embedUrl: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6051.581436593685!2d-74.23681722393101!3d40.67857997139864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5ca13190f5%3A0xd8ccec80658d4f7f!2sCenter%20for%20Academic%20Success%20%5BCAS%5D!5e0!3m2!1sen!2sus!4v1773685994760!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
   },
   {
   title: "Harwood Arena",
   address: "Harwood Arena, Kean University, 1000 Morris Ave, Union, NJ 07083",
-  embedUrl: <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6051.581436593685!2d-74.23681722393101!3d40.67857997139864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5cee982485%3A0xb89be333a3fc3397!2sHarwood%20Arena!5e0!3m2!1sen!2sus!4v1773686140465!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  embedUrl: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6051.581436593685!2d-74.23681722393101!3d40.67857997139864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5cee982485%3A0xb89be333a3fc3397!2sHarwood%20Arena!5e0!3m2!1sen!2sus!4v1773686140465!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
   },
   {
     title: "Wilkins Theatre",
     address: "1000 Morris Ave, Union, NJ 07083",
-    embedUrl: <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4507.468295897964!2d-74.23570029056846!3d40.67835574984721!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5a50cd02db%3A0x22b32f45c7674622!2sWilkins%20Theatre!5e0!3m2!1sen!2sus!4v1773687193321!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>,
+    embedUrl: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4507.468295897964!2d-74.23570029056846!3d40.67835574984721!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5a50cd02db%3A0x22b32f45c7674622!2sWilkins%20Theatre!5e0!3m2!1sen!2sus!4v1773687193321!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
   },
   {
   title: "Vaughn-Eames Hall",
   address: "1000 Morris Ave, Union, NJ 07083",
-  embedUrl: <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2852.714419863423!2d-74.23490406216058!3d40.67752076120921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5a95a6f8b9%3A0x52b36d39fdc974c0!2sVaughn-Eames%20Hall!5e0!3m2!1sen!2sus!4v1774464891988!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  embedUrl: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2852.714419863423!2d-74.23490406216058!3d40.67752076120921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5a95a6f8b9%3A0x52b36d39fdc974c0!2sVaughn-Eames%20Hall!5e0!3m2!1sen!2sus!4v1774464891988!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
   },
   {
   title: "Hennings Hall",
   address: "Kean University, Hennings Hall, Kean University, 1000 Morris Ave, Union, NJ 07083",
-  embedUrl: <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3585.7413202813336!2d-74.23665878105565!3d40.68047187138274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5b3fd5fde3%3A0x244e308e4046d7f5!2sHennings%20Hall!5e0!3m2!1sen!2sus!4v1774464990116!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  embedUrl: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3585.7413202813336!2d-74.23665878105565!3d40.68047187138274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5b3fd5fde3%3A0x244e308e4046d7f5!2sHennings%20Hall!5e0!3m2!1sen!2sus!4v1774464990116!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
   },
   {
   title: "Hutchinson Hall",
   address: "Kean University, Hutchinson Hall, 1000 Morris Ave, Union, NJ 07083",
-  embedUrl: <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6051.479200257821!2d-74.23622122393093!3d40.679706171398486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5ca13190f5%3A0x3ce9b9786df4b4da!2sHutchinson%20Hall!5e0!3m2!1sen!2sus!4v1774465103664!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-  },
+  embedUrl: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6051.479200257821!2d-74.23622122393093!3d40.679706171398486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ad5ca13190f5%3A0x3ce9b9786df4b4da!2sHutchinson%20Hall!5e0!3m2!1sen!2sus!4v1774465103664!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+  }
 ];
