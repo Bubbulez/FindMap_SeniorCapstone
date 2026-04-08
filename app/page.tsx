dc6718d0c2398cc1eaff6b08dd50bc7e3eed6a3a
Nahla Wright <88988827+NahlaWright@users.noreply.github.com>
2026-04-08 19:19:03 +0000

    lalala

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import "./styles/home.css";
 
 export default function Home() {
   const router = useRouter();
