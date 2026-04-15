0ff5e499417dc96b030c3586da1fc0466c2bc1a8
Deli <A.Delali.Doe@gmail.com>
2026-04-15 19:31:19 +0000

    home button added

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,11 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import "./styles/home.css";
+import Link from "next/link";
+
+<Link href="/" className="home-button">
+  Home
+</Link>
 
 export default function Home() {
   const router = useRouter();
